4c94863a89a6779408cf396c2ceb31fe71c24938
jckdm <jack.adam@yale.edu>
2021-03-23 00:34:04 -0400

    wow there's two buildings now

--- skyline.js
+++ skyline.js
@@ -30,10 +30,12 @@ times = {
 let bg;
 let bbg;
 
-const buildings = [
-  { 'start': null },
-  { 'start': null }
+let buildings = [
+  { 'start': null, 'id': 'empire' },
+  { 'start': null, 'id': 'chrysler' }
 ];
+
+func = (a, b) => 0.5 - Math.random()
 
 const w = $(window).width() - 17.5;
 const h = $(window).height() - 17.5;
@@ -60,30 +62,54 @@ setup = () => {
     ws.push(ws[i] / 2.0);
   }
 
+  buildings = buildings.sort(func);
+
   buildings[0]['start'] = -ws[0];
   buildings[1]['start'] = (w / -2.0) - ws[0];
 }
 
-let step = 1.0;
+let step = 0.75;
 speed = (v) => step = +v
 
 draw = () => {
-
   for (b of buildings) {
 
     b['start'] = (b['start'] + step >= w) ? -ws[0] : (b['start'] + step) % w;
 
-    triangle(b['start'] + ws[1], hs[3], b['start'] + ws[1] - ws[4], h - hs[0] - hs[2] - (hs[4] * 4), b['start'] + ws[1] + ws[4], h - hs[0] - hs[2] - (hs[4] * 4));
+    if (b.id == 'chrysler') {
+      triangle(b['start'] + ws[1], hs[3], b['start'] + ws[1] - ws[4], h - hs[0] - hs[2] - (hs[4] * 4), b['start'] + ws[1] + ws[4], h - hs[0] - hs[2] - (hs[4] * 4));
 
-    circle(b['start'] + ws[1], h - hs[0] - hs[2] - (hs[4] * 4), ws[0] - ws[2] - (ws[3] * 4));
-    circle(b['start'] + ws[1], h - hs[0] - hs[2] - (hs[4] * 3), ws[0] - ws[2] - (ws[3] * 3));
-    circle(b['start'] + ws[1], h - hs[0] - hs[2] - (hs[4] * 2), ws[0] - ws[2] - (ws[3] * 2));
-    circle(b['start'] + ws[1], h - hs[0] - hs[2] - hs[4], ws[0] - ws[2] - ws[3]);
-    circle(b['start'] + ws[1], h - hs[0] - hs[2], ws[0] - ws[2]);
+      circle(b['start'] + ws[1], h - hs[0] - hs[2] - (hs[4] * 4), ws[0] - ws[2] - (ws[3] * 4));
+      circle(b['start'] + ws[1], h - hs[0] - hs[2] - (hs[4] * 3), ws[0] - ws[2] - (ws[3] * 3));
+      circle(b['start'] + ws[1], h - hs[0] - hs[2] - (hs[4] * 2), ws[0] - ws[2] - (ws[3] * 2));
+      circle(b['start'] + ws[1], h - hs[0] - hs[2] - hs[4], ws[0] - ws[2] - ws[3]);
+      circle(b['start'] + ws[1], h - hs[0] - hs[2], ws[0] - ws[2]);
 
-    rect(b['start'] + ws[3], h - hs[0] - hs[2], ws[0] - ws[2], hs[0] - hs[2], 5, 5, 0, 0);
-    rect(b['start'] + ws[2], hs[0] - hs[3], ws[1], hs[0] - hs[2]);
-    rect(b['start'], hs[0], ws[0], hs[0], 20, 20, 0, 0);
+      rect(b['start'] + ws[3], h - hs[0] - hs[2], ws[0] - ws[2], hs[0] - hs[2], 5, 5, 0, 0);
+      rect(b['start'], hs[0], ws[0], hs[0], 20, 20, 0, 0);
+
+      rect(b['start'] + ws[2], h - (hs[0] + hs[2] + hs[4]), ws[1], hs[0] + hs[2] + hs[4], 30, 30, 0, 0);
+      rect(b['start'] + ws[2] + ws[3], h - (hs[0] + hs[2]), ws[2], hs[0] + hs[2], 20, 20, 0, 0);
+
+      rect(b['start'] + ws[1] + ws[3], hs[0] + hs[4], ws[2], hs[0] + hs[3], 0, 15, 0, 0);
+      rect(b['start'] + ws[3], hs[0] + hs[4], ws[2], hs[0] + hs[3], 15, 0, 0, 0);
+    }
+    else if (b.id == 'empire') {
+      triangle( b['start'] + ws[0], hs[4], b['start'] + ws[0] - ws[3], h - hs[0] - hs[1] - hs[4], b['start'] + ws[0] + ws[3], h - hs[0] - hs[1] - hs[4]);
+
+      rect(b['start'] + ws[1] + ws[3], h - hs[0] - hs[1] - hs[4], ws[1] + ws[2], hs[4]);
+
+      rect(b['start'] + ws[1] + ws[4], h - hs[0] - hs[1], ws[0] - ws[4] * 2, hs[0] - hs[1]);
+      rect(b['start'] + ws[1] + ws[4], h - hs[0] - hs[1], ws[1] - ws[2], hs[0] - hs[1]);
+      rect(b['start'] + ws[0] + ws[3] + ws[4], h - hs[0] - hs[1], ws[1] - ws[2], hs[0] - hs[1]);
+
+      rect(b['start'] + ws[1], h - hs[0], ws[0], hs[0], 5, 5, 0, 0);
+      rect(b['start'] + ws[2], h - hs[2], ws[0] * 1.5, hs[2], 5, 5, 0, 0);
+      rect(b['start'], h - hs[3], ws[0] * 2, hs[3], 10, 10, 0, 0);
+
+      rect(b['start'] + ws[1], h - hs[0], ws[2], hs[0] - hs[2]);
+      rect(b['start'] + ws[0] + ws[1] - ws[2], h - hs[0], ws[2], hs[0] - hs[2]);
+    }
   }
   fill(bbg[0], bbg[1], bbg[2]);
   stroke(bg[0], bg[1], bg[2]);
